9f09b3dfc72195d543d341e05d05c61f44f05c60
Carl Reiner <creiner1@gmail.com>
2015-11-02 20:22:43 -0800

    stopped stream when user disconnects, cleaned up index.js code

--- index.js
+++ index.js
@@ -23,42 +23,22 @@ app.route('/')
     res.render('index');
   });
 
-app.get('/stream', function(req, res) {
-  console.log(
-    typeof req.query.swLng,
-    req.query.swLat,
-    req.query.neLng,
-    req.query.neLat
-  );
-  res.end();
-});
-
-//location bounds: SW first, NE second, [lng, lat] format
-var seattle = ['-122.354','47.6','-122.32','47.63'];
-
-//Create stream
-// var stream = twitter.stream('statuses/filter', { locations: seattle });
-
 //Server-side socket.io to emit tweets
 io.on('connect', function(socket) {
-  // create client specific stream
-  // on stream tweet, send to socket
   var stream = null;
-
   socket.on('location', function(newLocation) {
-    console.log('In the location handler');
-    //Initialize the Twitter stream based on user input
     if(stream) {
       stream.stop();
     }
     stream = twitter.stream('statuses/filter', { locations: newLocation.coords });
-    //Emit tweets from the Twitter stream
     stream.on('tweet', function(tweet) {
       console.log('Emitting tweet');
       socket.emit('tweets', tweet);
     });
   });
-
+  socket.on('disconnect', function() {
+    stream.stop();
+  })
 });
 
 server.listen(port, function() {
